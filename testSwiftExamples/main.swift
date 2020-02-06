bea06b856f0e9ae237d948b721c933e178a78e4a
prana <prakashrana9847067614@gmail.com>
2020-02-06 17:24:53 -0500

    Initial Commit

---
+++ testSwiftExamples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testSwiftExamples
+//
+//  Created by Prakash on 06/02/20.
+//  Copyright © 2020 Prakash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
